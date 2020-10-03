9b34d07a8f9301da47c148b7753448c30e5f54da
caglarmert <mert.caglar@metu.edu.tr>
2020-10-03 23:23:33 +0300

    Added train.py for easy model training

---
+++ train.py
@@ -0,0 +1,47 @@
+import gym
+import numpy as np
+
+import argparse
+
+import jsbsim
+import gym_jsbsim
+
+from stable_baselines.ddpg.policies import MlpPolicy
+from stable_baselines.common.vec_env import DummyVecEnv
+from stable_baselines.ddpg.noise import NormalActionNoise, OrnsteinUhlenbeckActionNoise, AdaptiveParamNoiseSpec
+from stable_baselines import DDPG
+from stable_baselines import logger
+
+# Necessary for it to generate tensorboard log files during the run.  Note that
+# in the docker file I set OPENAI_LOGDIR and OPENAI_LOG_FORMAT environment variables
+# to generate the kind of logs that I want.
+logger.configure()
+
+env = gym.make('JSBSim-TurnHeadingControlTask-Cessna172P-Shaping.STANDARD-NoFG-v0')
+env = DummyVecEnv([lambda: env])
+
+# the noise objects for DDPG
+n_actions = env.action_space.shape[-1]
+param_noise = None
+action_noise = OrnsteinUhlenbeckActionNoise(mean=np.zeros(n_actions), sigma=float(0.5) * np.ones(n_actions))
+
+try:
+    model = DDPG.load("model/ddpg_fg_turnheading", env=env, tensorboard_log="model/tensorboard/")
+    model.set_env( env )
+    print("Model exists")
+
+except ValueError:  # Model doesn't exist
+    print("Model doesn't exists, training new one")
+    model = DDPG(MlpPolicy, env, verbose=1, param_noise=param_noise, action_noise=action_noise, normalize_observations=True, tensorboard_log="model/tensorboard/")
+    model.learn(total_timesteps=2e4)
+    model.save("model/ddpg_fg_turnheading")
+
+
+env = gym.make('JSBSim-TurnHeadingControlTask-Cessna172P-Shaping.STANDARD-FG-v0')
+env.reset()
+done = False
+
+while True:
+   action = env.action_space.sample()
+   state, reward, done, _ = env.step(action)
+   env.render(mode='flightgear')
